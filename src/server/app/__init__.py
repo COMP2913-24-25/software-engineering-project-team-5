5eeeef1f34b479b7394ad628e4498f90b5cbd138
tahmid-Uddin <146216062+tahmid-Uddin@users.noreply.github.com>
2025-02-21 03:26:43 +0000

    Moved flask files to server folder

---
+++ src/server/app/__init__.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_admin import Admin
+from flask_babel import Babel
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
+from flask_wtf.csrf import CSRFProtect
+
+def get_locale():
+    if request.args.get("lang"):
+        session["lang"] = request.args.get("lang")
+    return session.get("lang", "en")
+
+app = Flask(__name__)
+app.config.from_object("config")
+
+Bootstrap(app)
+csrf = CSRFProtect(app)
+
+babel = Babel(app, locale_selector=get_locale)
+admin = Admin(app, template_mode="bootstrap4")
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from app.models import User  
+from app import views, models 
+
+@login_manager.user_loader
+def load_user(id):
+    if id is None:
+        return None
+    try:
+        user = User.query.get(int(id))
+        return user
+    except ValueError:
+        return None
