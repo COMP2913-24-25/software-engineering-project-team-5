760d430e4e69a23c3089bd543fb7ec77fab027d7
tahmid-Uddin <146216062+tahmid-Uddin@users.noreply.github.com>
2025-02-23 13:33:12 +0000

    Updated tests to reflect switch to React

--- src/server/tests/test_login.py
+++ src/server/tests/test_login.py
@@ -2,11 +2,12 @@ import sys
 import os
 import pytest
 import datetime
+import json
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import app, db 
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import current_user, login_user
+from flask_login import current_user
 from app.models import User 
 
 @pytest.fixture
@@ -36,35 +37,34 @@ def client():
             db.session.add(test_user)
             db.session.commit()
             
-            # Runs all the tests defined below
             yield client
             
-            # Cleans up the database - removes all data, drops all tables
             db.session.remove()
             db.drop_all()
 
-# Define all tests as: def test_<test_name>(client):
-def test_login_page_loads(client):
-    """
-    Test that the login page loads correctly
-    """
-    
-    response = client.get("/login")
-    
-    #Correctly loaded response code
-    assert response.status_code == 200
 
 def test_login_valid_email(client):
     """
     Test login with valid email
     """
     
-    response = client.post("/login", data={
-        "email_or_username": "test@gmail.com",
-        "password": "PASSword123@"
-    }, follow_redirects=True)
+    # Creates a test response as a json object
+    response = client.post("/api/login", 
+        json={
+            "email_or_username": "test@gmail.com",
+            "password": "PASSword123@"
+        },
+        content_type='application/json'
+    )
     
+    # Gets data from the server
+    server_response = json.loads(response.data)
+    
+    # Checks if the correct response code is returned
     assert response.status_code == 200
+    
+    # Checks if the correct response is returned and that the user is successfully logged in
+    assert server_response["message"] == "Login successful"
     assert current_user.is_authenticated
 
 
@@ -72,40 +72,72 @@ def test_login_valid_username(client):
     """
     Test login with valid username 
     """
+    response = client.post("/api/login", 
+        json={
+            "email_or_username": "testuser",
+            "password": "PASSword123@"
+        },
+        content_type='application/json'
+    )
     
-    response = client.post("/login", data={
-        "email_or_username": "testuser",
-        "password": "PASSword123@"
-    }, follow_redirects=True)
+    # Gets data from the server
+    server_response = json.loads(response.data)
     
+    # Checks if the correct response code is returned
     assert response.status_code == 200
+    
+    # Checks if the correct response is returned and that the user is successfully logged in
+    assert server_response["message"] == "Login successful"
     assert current_user.is_authenticated
+
 
 def test_login_invalid_password(client):
     """
     Test login with invalid password
     """
+    response = client.post("/api/login", 
+        json={
+            "email_or_username": "test@gmail.com",
+            "password": "wrongpassword"
+        },
+        content_type='application/json'
+    )
     
-    response = client.post("/login", data={
-        "email_or_username": "test@gmail.com",
-        "password": "wrongpassword"
-    }, follow_redirects=True)
+    # Gets data from the server
+    server_response = json.loads(response.data)
     
-    assert response.status_code == 200
-    assert b"Invalid email or password" in response.data
+    # Checks if the correct response code is returned
+    assert response.status_code == 400
+    
+    # Checks if there is a password error and that the correct error message is returned
+    assert "password" in server_response["errors"]
+    assert "Incorrect password" in server_response["errors"]["password"]
+    
+    # Checks that the user is not logged in
     assert not current_user.is_authenticated
+
 
 def test_login_invalid_user(client):
     """
     Test login with non-existent user
     """
+    response = client.post("/api/login", 
+        json={
+            "email_or_username": "nonexistent@example.com",
+            "password": "password123"
+        },
+        content_type='application/json'
+    )
     
-    response = client.post("/login", data={
-        "email_or_username": "nonexistent@example.com",
-        "password": "password123"
-    }, follow_redirects=True)
+    # Gets data from the server
+    server_response = json.loads(response.data)
     
-    assert response.status_code == 200
-    assert b"Invalid email or password" in response.data
+    # Checks if the correct response code is returned
+    assert response.status_code == 400
+    
+    # Checks if there is an email or username error and that the correct error message is returned
+    assert "email_or_username" in server_response["errors"]
+    assert "User not found" in server_response["errors"]["email_or_username"]
+    
+    # Checks that the user is not logged in
     assert not current_user.is_authenticated
-    
--- src/server/tests/test_signup.py
+++ src/server/tests/test_signup.py
@@ -2,13 +2,13 @@ import sys
 import os
 import pytest
 import datetime
+import json
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from app import app, db 
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import current_user, login_user
+from flask_login import current_user
 from app.models import User 
-from flask import get_flashed_messages, request
 
 @pytest.fixture
 def client():
@@ -25,101 +25,165 @@ def client():
             db.session.remove()
             db.drop_all()
 
-def test_signup_page_loads(client):
-    """
-    Test that the signup page loads correctly
-    """
-    
-    response = client.get("/signup")
-    assert response.status_code == 200
-    
 def test_signup_valid(client):
     """
-    Test login with valid details 
+    Test signup with valid details 
     """
+
+    # User signups up with valid details
+    response = client.post("/api/signup", 
+        json={
+            "username": "testuser",
+            "password": "PASSword123@",
+            "email": "test@gmail.com",
+            "first_name": "John",
+            "middle_name": "Joe",
+            "surname": "Doe",
+            "DOB": "2000-01-01",
+            "passwordConfirmation": "PASSword123@"
+        },
+        content_type='application/json'
+    )
     
-    response = client.post("/signup", data={
-        "username":"testuser",
-        "password":"PASSword123@",
-        "email":"test@gmail.com",
-        "first_name":"John",
-        "middle_name":"Joe",
-        "surname":"Doe",
-        "DOB":datetime.date(2000, 1, 1),
-        "passwordConfirmation":"PASSword123@"
-    }, follow_redirects=True)
-    
-    for key, value in request.form.items():
-        print(f"{key}: {value}")
-    
+    server_response = json.loads(response.data)
+    # Checks that the correct reponse code is returned
     assert response.status_code == 200
+    
+    # Check if the message in the response matches the expected message
+    assert server_response["message"] == "User created successfully"
+    
+    # Check if the user is authenticated
     assert current_user.is_authenticated
     
-def test_signup_invalid_name(client):
-    """
-    Test login with invalid name
-    """
-    
-    response = client.post("/signup", data={
-        "username":"testuser",
-        "password":generate_password_hash("PASSword123@"),
-        "email":"test@gmail.com",
-        "first_name":"John12",
-        "middle_name":"Joe34@",
-        "surname":"Doe56",
-        "DOB":datetime.date(1990, 1, 1),
-        "passwordConfirmation":generate_password_hash("PASSword123@")
-    }, follow_redirects=True)
-    
-    for key, value in request.form.items():
-        print(f"{key}: {value}")
-    
-    assert response.status_code == 200
-    assert b"Name contains invalid characters." in response.data
-    
+    # Check if the data in the database matches the user input
+    user = User.query.filter_by(Email="test@gmail.com").first()
+    assert user is not None
+    assert user.Username == "testuser"
+    assert user.First_name == "John"
+    assert user.Middle_name == "Joe"
+    assert user.Surname == "Doe"
+    assert user.DOB == datetime.date(2000, 1, 1)
 
-def test_login_invalid_DOB(client):
-    """
-    Test login with invalid date of birth
-    """
-    
-    response = client.post("/signup", data={
-        "username":"testuser",
-        "password":generate_password_hash("PASSword123@"),
-        "email":"test@gmail.com",
-        "first_name":"John",
-        "middle_name":"Joe",
-        "surname":"Doe",
-        "DOB":datetime.date(3000, 1, 1),
-        "passwordConfirmation":generate_password_hash("PASSword123@")
-    }, follow_redirects=True)
-    
-    for key, value in request.form.items():
-        print(f"{key}: {value}")
-    
-    assert response.status_code == 200
-    assert b"Invalid Date of Birth." in response.data
-    
 
-def test_login_invalid_password(client):
+def test_signup_existing_email(client):
     """
-    Test login with invalid password
+    Test signup with existing email
+    """
+    # Create a user
+    test_user = User(
+        Username="existinguser",
+        Password=generate_password_hash("PASSword123@"),
+        Email="test@gmail.com",
+        First_name="John",
+        Surname="Doe",
+        DOB=datetime.date(1990, 1, 1),
+        Level_of_access=1,
+        Is_expert=False
+    )
+    db.session.add(test_user)
+    db.session.commit()
+
+    # User tries to make another account with the same email
+    response = client.post("/api/signup", 
+        json={
+            "username": "newuser",
+            "password": "PASSword123@",
+            "email": "test@gmail.com",
+            "first_name": "Jane",
+            "middle_name": "Juniper",
+            "surname": "Joe",
+            "DOB": "2000-01-01",
+            "passwordConfirmation": "PASSword123@"
+        },
+        content_type='application/json'
+    )
+    
+    # Gets response from the server
+    server_response = json.loads(response.data)
+    
+    # Checks if correct response code is returned
+    assert response.status_code == 400
+    
+    # Checks if there is an error with the email
+    assert "email" in server_response["errors"]
+    
+    # Check if the correct error message for email has been returned
+    assert "Email already exists" in server_response["errors"]["email"]
+
+
+def test_signup_existing_username(client):
+    """
+    Test signup with existing username
+    """
+    # Create a user
+    test_user = User(
+        Username="testuser",
+        Password=generate_password_hash("PASSword123@"),
+        Email="existing@gmail.com",
+        First_name="John",
+        Surname="Doe",
+        DOB=datetime.date(1990, 1, 1),
+        Level_of_access=1,
+        Is_expert=False
+    )
+    db.session.add(test_user)
+    db.session.commit()
+
+    # User tries to create another account with the same username
+    response = client.post("/api/signup", 
+        json={
+            "username": "testuser",
+            "password": "PASSword123@",
+            "email": "new@gmail.com",
+            "first_name": "Jane",
+            "middle_name": "Joe",
+            "surname": "Doe",
+            "DOB": "2000-01-01",
+            "passwordConfirmation": "PASSword123@"
+        },
+        content_type='application/json'
+    )
+    
+    # Gets response from the server
+    server_response = json.loads(response.data)
+    
+    # Checks if correct response code is returned
+    assert response.status_code == 400
+    
+    # Checks if there is an username error, and that the correct error message
+    # has been returned
+    assert "username" in server_response["errors"]
+    assert "Username already exists" in server_response["errors"]["username"]
+    
+    
+def test_signup_password_mismatch(client):
+    """
+    Test signup with mismatched passwords
     """
     
-    response = client.post("/signup", data={
-        "username":"testuser",
-        "password":"invalidpassword",
-        "email":"test@gmail.com",
-        "first_name":"John",
-        "middle_name":"Joe",
-        "surname":"Doe",
-        "DOB":datetime.date(3000, 1, 1),
-        "passwordConfirmation":"invalidpassword"
-    }, follow_redirects=True)
+    response = client.post("/api/signup", 
+        json={
+            "username": "testuser",
+            "password": "PASSword123@",
+            "email": "test@gmail.com",
+            "first_name": "John",
+            "middle_name": "Joe",
+            "surname": "Doe",
+            "DOB": "2000-01-01",
+            "passwordConfirmation": "DifferentPassword123@"
+        },
+        content_type='application/json'
+    )
+    
+    # Gets response from the server
+    server_response = json.loads(response.data)
+    
+    # Checks if correct response code is returned
+    assert response.status_code == 400
+    
+    # Checks if there is a passwordConfirmation error, and that the correct error
+    # message has been returned
+    assert "passwordConfirmation" in server_response["errors"]
+    assert "Passwords do not match" in server_response["errors"]["passwordConfirmation"]
 
-    
-    assert response.status_code == 200
-    assert b"Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character." in response.data
-    
 
-    
