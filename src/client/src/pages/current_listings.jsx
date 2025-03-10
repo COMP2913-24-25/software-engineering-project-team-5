e55bc9412e5fd32b6b2a732916afc70fcdbe41c7
Kavisha612 <sc23k2g@leeds.ac.uk>
2025-03-10 05:00:33 +0000

    Current_listing page implemented with basic price filter

--- src/client/src/pages/current_listings.jsx
+++ src/client/src/pages/current_listings.jsx
@@ -5,15 +5,22 @@ import { useNavigate } from "react-router-dom";
 import {useCSRF } from "../App"; // Calls the user
 import Listing_item from "../components/listing_items";
 import "../App.css";
-import ItemListing from "../components/itemlisting";
+// import ItemListing from "../components/itemlisting";
 import { ChevronRight } from "lucide-react";
+import Filter_component from "../components/Filter_Sidebar";
 
 export default function CurrentListings({searchQuery}) {
     const [listings, set_listings] = useState([]);
+    const [price_filtered_listings, setprice_filtered_listings] = useState([]); // Stores price-filtered data
+    // const [minPrice, setMinPrice] = useState(4);
+    // const [maxPrice, setMaxPrice] = useState(100000); 
+    // max min will be used when price filtering is through sliding convention
+
     const navigate = useNavigate();
     const [middle_type, set_middletype] = useState([]);
     const [types, set_types] = useState([]);
     const { csrfToken } = useCSRF();
+
 
 
     useEffect(() => {
@@ -32,8 +39,7 @@ export default function CurrentListings({searchQuery}) {
                     }
                 );
 
-                //fetching middle-types or types table is causing error. 
-                // working on it
+               
 
                 const listings_data = await listings_response.json();
 
@@ -43,13 +49,13 @@ export default function CurrentListings({searchQuery}) {
 
                        
                     //Filter works only for a single word searchQuery
-
+                    //Need to move into database for filtering
                     //Filtering by name
                     //Item name : adidas shoe, then it would work for any letters entered in the right sequence
                     // works for : adi, a, adidas, s, sho, shoe
                     // does not work for : did, ho, adidas shoe
                     // should work for : adidas shoe
-                    const filteredListings = searchQuery 
+                    const filteredListings_search = searchQuery 
                     ? listings_data.filter((listings) =>{
                         if (!listings?.Listing_name) return false;
                             
@@ -61,12 +67,11 @@ export default function CurrentListings({searchQuery}) {
                 })
                     : listings_data;
                    
+                // set only by search Query
+                set_listings(filteredListings_search);
+                setprice_filtered_listings(filteredListings_search)
+                // console.log("Fetched Listings: ", filteredListings); 
 
-                set_listings(filteredListings);
-                console.log("Fetched Listings: ", filteredListings); 
-                                            
-
-                      // Set all listings without filtering
                 } else {
                     console.error("Failed to fetch listings");
                 }
@@ -74,44 +79,16 @@ export default function CurrentListings({searchQuery}) {
                 console.error("Network error: ", error);
             }
         };
-
-    
         fetchListings();
     }, [searchQuery]);
 
-    // useEffect(() => {
-    //     const fetchListings = async () => {
-    //         try {
-    //             const listings_response = await fetch("http://localhost:5000/api/get-items", {
-    //                 method: "GET",
-    //                 credentials: "include",
-    //             });
 
-    //             const listings_data = await listings_response.json();
 
-    //             if (listings_response.ok) {
-    //                 const filtered_search = searchQuery
-    //                 ? listings_data.filter((listing) =>
-    //                     listing.Listing_name && listing.Listing_name.toLowerCase().includes(searchQuery.toLowerCase()) 
-    //                 ) : listings_data;
-    //                 console.log("Filtered Listings: ", filtered_search);  // Log filtered search
-
-    //                 set_listings(filtered_search);
-    //                 // set_listings(filteredListings); 
-    //             } else {
-    //                 console.error("Failed to fetch listings");
-    //             }
-    //         } catch (error) {
-    //             console.error("Network error: ", error);
-    //         }
-    //     };
-
-    //     fetchListings();
-    // }, [searchQuery]);
-
+    //no committed yet
     const handleItemClick = (item) => {
         navigate("/enlarge_listing", { state: { item } });
     };
+
 
     const calculate_time_remaining = (availableUntil) => {
         const endTime = new Date(availableUntil).getTime();
@@ -145,50 +122,71 @@ export default function CurrentListings({searchQuery}) {
     
             return () => clearInterval(interval); // Cleanup on unmount
         }, []);
+        
+    const [sidebarVisible, setSidebarVisible] = useState(false);
+   
+    const toggleSidebar = () => {
+        setSidebarVisible((prev) => !prev);
+        };
+
+
+    const handle_filtered_listings = (filtered_listings) => {
+        // console.log("Filtered being set ", filtered_listings);
+
+        setprice_filtered_listings(filtered_listings);
+
+    };
+
+    // useEffect(() => {
+    //     console.log("Filtered after set", price_filtered_listings);
+    // }, [price_filtered_listings]);
+
 
     return (
         
         <div className="container mx-auto p-8 text-center">
+             {/* <div>
+            <label>Min Price: </label>
+            <input
+                type="number"
+                value={minPrice}
+                onChange={(e) => setMinPrice(e.target.value)}
+            />
+            
+            <label>Max Price: </label>
+            <input
+                type="number"
+                value={maxPrice}
+                onChange={(e) => setMaxPrice(e.target.value)}
+            />
+        </div> */}
 
-      
-    <h3 className="text-lg mb-4">Search Query: {searchQuery}</h3>
-
+            <Filter_component update_listings={handle_filtered_listings} listings = {listings} />
+            {/* <h3 className="text-lg mb-4">Search Query: {searchQuery}</h3> */}
             <h1 className="text-3xl font-bold mb-6">Current Listings</h1>
-            {listings.length === 0 ? (
+            {price_filtered_listings.length === 0 ? (
                 <p className="text-gray-600">No current listings available.</p>
             ) : (
-                listings.map((item) => (
+                price_filtered_listings.map((item) => (
                     <div 
                     key={item.id} 
                     className="bid-item p-4 border rounded-lg shadow-md flex gap-2 bg-yellow-100 relative" // Added relative for positioning arrow
-                >
-                    {/* Navigation Arrow Button */}
-                    <button 
-                        className="absolute top-2 right-2 bg-gray-200 p-1 rounded-full hover:bg-gray-300" 
-                        onClick={() => handleItemClick(item)}
-                    >
-                        <ChevronRight size={20} />
-                    </button>
-                
-                    <div className="flex items-center gap-2 p-4 border rounded-lg shadow-md bg-green-100 w-full">
+                    > 
 
-                    <ItemListing
-                                key={item.Item_id}
-                                image={item.Image}
-                                title={item.Listing_name}
-                                seller={item.Seller_name}
-                                description={item.Description}
-                                labels={[
-                                    `Current Bid: £ ${(Number(item.Current_bid) > Number(item.Min_price)) ? Number(item.Current_bid).toFixed(2) : Number(item.Min_price).toFixed(2)}`,
-                                    `Time Remaining: ${item.timeRemaining || calculate_time_remaining(item.Available_until)}`,
-                                ]}
-                                
-                            />
+                    {/* Navigation Arrow Button : not working since enlarge lsiting not pushed */}
+                        <button 
+                            className="absolute top-2 right-2 bg-gray-200 p-1 rounded-full hover:bg-gray-300" 
+                            onClick={() => handleItemClick(item)}
+                        >
+                            <ChevronRight size={20} />
+                        </button>
+                    
+                        <div className="flex items-center gap-2 p-4 border rounded-lg shadow-md bg-green-100 w-full">
+                        <Listing_item key={item.id} item={item} />
+                        </div>
                     </div>
-                </div>
-
-                ))
-            )}
+                    ))
+                )}
         </div>
     );
 }
