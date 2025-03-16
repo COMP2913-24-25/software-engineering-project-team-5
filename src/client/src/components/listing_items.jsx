1739b8d85d403b4842698eeb49e80ede8aa84659
Ali Hesham Osman <146213220+AliiOsman@users.noreply.github.com>
2025-03-16 00:39:19 +0000

    Update listing_items.jsx

--- src/client/src/components/listing_items.jsx
+++ src/client/src/components/listing_items.jsx
@@ -14,7 +14,6 @@ const Listing_item = (props) => {
     const handleClick = () => {
         
         {
-            // Assuming `item.Authentication_request` is the property to check if it's under review
             if (item.Authentication_request === true && item.Expert_id !== null) {
                 // Under review item
                 let url = "/authrequest/" + encodeURIComponent(item.Listing_name) + "/" + item.Item_id; 
