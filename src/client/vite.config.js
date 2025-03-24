85a4771db281e951a79938e2f6b5ea4ae2e428c9
tahmid-Uddin <146216062+tahmid-Uddin@users.noreply.github.com>
2025-03-24 02:37:29 +0000

    Added Build Configs

--- src/client/vite.config.js
+++ src/client/vite.config.js
@@ -1,13 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 
-// https://vitejs.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
     plugins: [react(), tailwindcss()],
     test: {
         environment: "jsdom",
         globals: true,
         setupFiles: "./src/tests/setup.js",
+    },
+    build: {
+        outDir: "dist", // Output directory for the build
+        emptyOutDir: true, // Clear the output directory before building
+        rollupOptions: {
+            input: resolve(__dirname, "index.html"), // Single entry point for SPA
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return id.split("node_modules/")[1].split("/")[0];
+                    }
+                },
+            },
+        },
     },
 });
