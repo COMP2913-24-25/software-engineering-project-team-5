f4a2863a43fd7bbeedc03e119fe34e54cc8d9440
tahmid-Uddin <146216062+tahmid-Uddin@users.noreply.github.com>
2025-03-24 02:40:47 +0000

    Updated run.py to route from production build correctly

--- src/server/run.py
+++ src/server/run.py
@@ -1,4 +1,12 @@
 from app import app, socketio
+from flask import send_from_directory
+
+
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def serve(path):
+    return send_from_directory("dist", "index.html")
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
